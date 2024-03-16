8d00428fe034c4c906e103fcfea38167a4a28fd6
aish <aishnikam31@gmail.com>
2024-03-16 23:45:31 +0530

    slider with carousel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar' 
 import Menu from './components/Menu/Menu';
+import Slider from './components/Slider/Slider';
 
 const App = () => {
   const [menuOpen, setMenuOpen] = React.useState(false);
@@ -9,6 +10,8 @@ const App = () => {
     <div>
     <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Slider />
+
     </div>
   )
 }
---
+++ frontend/src/components/Slider/Slider.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './Slider.css';
+import { useState } from 'react';
+
+const Slider = () => {
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+      setActiveIndex(selectedIndex);
+    };
+
+  return (
+    <div>
+      <div className="carousel-container">
+      <Carousel activeIndex={activeIndex} onSelect={handleSelect} interval={2000}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src='https://m.media-amazon.com/images/I/61zAjw4bqPL._SX3000_.jpg'
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/G/31/img15/4th/sept/unrechero/8thslot/Tws_Tallhero_3000x1200._CB596103422_.jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/G/31/IMG20/Home/2023/BAU2023/ATFGW/Winter_GW_PC_Unrec._CB570776754_.jpg"
+
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+     
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Slider
